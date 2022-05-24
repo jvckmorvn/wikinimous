abb8d7b2b9ab99022b8bc8bfb129d6a4e8d6dd84
Jack Moran <jvckmorvn@gmail.com>
2022-05-24 16:44:42 +0100

    Minimally styled blog-like site

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+(1..10).each do |article|
+  Article.create(
+      title: Faker::Movie.title,
+      content: Faker::Lorem.words(number: 220).join(" ")
+  )
+end
